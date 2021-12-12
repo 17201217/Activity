0b4552aa279fc2bd0e262c50015cf2a4f93ae9d2
pjm <2557272503@qq.com>
2021-12-12 22:41:54 +0800

    新增工作流引擎的两种创建方式

--- src/test/java/com/learning/pjm/test/TestCreate.java
+++ src/test/java/com/learning/pjm/test/TestCreate.java
@@ -1,7 +1,9 @@
 package com.learning.pjm.test;
 
 import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
 import org.junit.Test;
 
 
@@ -11,8 +13,16 @@ public class TestCreate {
      */
     @Test
     public void testCreateDbTable() {
-        // 使用classpath下的activiti.cfg.xml中的配置创建processEngine
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        // 使用classpath下的activiti.cfg.xml中的配置创建processEngine，
+        // 注意默认方式时，activiti.cfg.xml中id叫processEngineConfiguration，class叫StandaloneProcessEngineConfiguration
+        /*ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        repositoryService.createDeployment();
+        System.out.println(processEngine);*/
+        // 一般创建方式
+        ProcessEngineConfiguration configuration = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml");
+        ProcessEngine processEngine = configuration.buildProcessEngine();
         System.out.println(processEngine);
+
     }
 }
